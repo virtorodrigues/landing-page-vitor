f2da9916b20f22833c8dc12ba6a042a02d5d527d
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-05-12 12:17:01 -0300

    fix newsletter project description

--- src/portfolioProjects/newsLetterPage.tsx
+++ src/portfolioProjects/newsLetterPage.tsx
@@ -3,20 +3,20 @@ export const newsLetterPage = {
   description: (
     <>
       <p>
-        Está página de newsletter foi criada com inspiração na newsletter da
-        rocketseat que achei muito bonita.
+        Esta página de newsletter foi criada com inspiração na página de
+        newsletter da rocketseat que achei muito bonita.
       </p>
       <p className="mt-4">
         Projeto criado com <span className="text-blue font-bold">NextJs</span>.
         Possúi validação completa no formulário feita com{" "}
         <span className="text-blue font-bold">React hook forms</span> +{" "}
         <span className="text-blue font-bold">Zod</span>. Toda a parte de
-        responsividade e estilização da página foi feita com muito cuidado
+        responsividade e estilização da página, foi feita com muito cuidado
         utilizando o <span className="text-blue font-bold">Tailwindcss</span>{" "}
-        que agora é recomendado pelo NextJs. Ao preencher o formuário uma
-        simulação para inscrição é criada e um toast feito em{" "}
+        que agora é recomendado pelo NextJs. Ao preencher o formuário é criada
+        uma simulação para inscrição e um toast utilizando{" "}
         <span className="text-blue font-bold">React Toastify</span> aparece em
-        tela, garantindo o sucesso da inscrição.
+        tela, exibindo para o usuário o sucesso da inscrição.
       </p>
     </>
   ),
