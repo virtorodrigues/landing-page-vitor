459a87f41704da8df680672ea27a93d502f39e83
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-06-22 18:22:04 -0300

    feat: add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://landing-page-vitor.vercel.app',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,24 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Vitor Rodrigues',
-  description: 'Um pouco mais sobre mim',
+  description:
+    'Bem vindo a minha Landing Page. Saiba um pouco mais de mim e veja alguns dos projetos que já criei',
+  openGraph: {
+    title: 'Vitor Rodrigues',
+    description:
+      'Bem vindo a minha Landing Page. Saiba um pouco mais de mim e veja alguns dos projetos que já criei',
+    url: process.env.NEXT_PUBLIC_API_URL_BASE,
+    siteName: 'Vitor Rodrigues',
+    images: [
+      {
+        url: './icon.png',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'pt-BR',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
