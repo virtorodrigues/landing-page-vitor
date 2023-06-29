de44d49a67c4a58e4925162b51a9d1c6d895b1ca
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-06-29 15:06:49 -0300

    refactory: change sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://landing-page-vitor.vercel.app',
+  siteUrl: 'https://vitorrodrigues.vercel.app/',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
